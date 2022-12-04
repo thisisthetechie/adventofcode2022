c0aef5f577c6eaf5cebab7db2f5051d0c7b8180c
Pete Mallam <pete.mallam@health.telstra.com>
2022-12-04 09:12:09 +0000

    Day 4

---
+++ day4-2.py
@@ -0,0 +1,19 @@
+## Advent of Code 2022
+## Day 4 Part 1
+import aoc_core as aoc
+import re
+import math
+
+f = aoc.fd
+totalcount = 0
+
+for x in f:
+    a = x.split(",")
+    lo1 = int(a[0].split("-")[0])
+    hi1 = int(a[0].split("-")[1])
+    lo2 = int(a[1].split("-")[0])
+    hi2 = int(a[1].split("-")[1])
+    if (((hi1 >= lo2) & (lo1 <= hi2)) | ((hi2 >= lo1) & (lo2 <= hi1))):
+        totalcount += 1
+
+print("The total count is: " + str(totalcount))
